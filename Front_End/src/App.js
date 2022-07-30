c8fc6ecef6ced02667f56818a749b12bae3d8e99
JoaoAlb2021 <joaorenato2015@gmail.com>
2022-07-30 18:35:07 -0500

    Agregado 'Body.jsx' y mejorado 'Header' y 'Footer'

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -1,4 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Body from './components/Body';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Body/>
       <Footer/>
     </div>
   );
---
+++ Front_End/src/components/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div className='text-center'>
+        <img className='img-fluid' src='https://manualusa.com/wp-content/uploads/2019/07/trabajo-para-inmigrantes.jpg' alt='img-broke'/>
+    </div>
+  )
+}
+export default Body
