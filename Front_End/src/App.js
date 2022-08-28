b4056925d10882d90f5bfa52c4fdc66d9a8c7371
ratel4150 <rratel76@gmail.com>
2022-08-28 16:53:35 +0200

    Termino Routing App

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -3,26 +3,31 @@ import Body from './components/Body/BannerSearch';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import FormCompany from './components/Forms/FormCompany';
-import Login from './components/Forms/Login';
+
 import Card from "./components/Migrants/Card";
 import BannerCompany from './components/Body/Forms/BannerCompany';
 import NavBar from './components/Body/Forms/NavBar';
 import BannerWorkPosition from './components/Body/BannerWorkPosition';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home"
+import Migrants from "./pages/Migrants"
+import Company from "./pages/Company"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Body/>
-      
-      <BannerWorkPosition/>
-      <BannerCompany/>
-      
-      <Card />
-      {/* <FormCompany/> */}
-      
-      {/* <Login/> */}
-      <Footer/>
+      <Routes>
+                  
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/migrants" element={<Migrants/>} />
+                    
+          <Route path="/company" element={<Company />} />
+          <Route path="/login" element={<Login/>} />
+                         
+        </Routes>
     </div>
   );
 }
--- Front_End/src/index.js
+++ Front_End/src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import {BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
