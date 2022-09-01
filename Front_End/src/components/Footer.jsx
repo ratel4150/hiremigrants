2d2893485bc59acdb3e51312ad5a0d452cde21d0
jorgelinad <donquisjorgelina92@gmail.com>
2022-09-01 19:25:07 +0200

    footer modificado

--- Front_End/src/components/Footer.jsx
+++ Front_End/src/components/Footer.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import { BsInstagram } from "react-icons/bs";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
@@ -8,34 +6,33 @@ import { BsLinkedin } from "react-icons/bs"
 
 function Footer() {
   return (
-    <Card className="text-center bg-dark text-white">
-      <Card.Header>
-        <Button variant="dark" size="sm" href="">
-          Migrants
-        </Button>
-        <Button variant="dark" size="sm" href="">
-          Company
-        </Button>
-        <Button variant="dark" size="sm" href="">
-          Login
-        </Button>
-        </Card.Header>
-      <Card.Body>
-        <Card.Img
+    <div className="text-center text-white" style={{
+      backgroundColor: "#369962",
+    }}>
+      <div class="btn-group p-3" role="group" aria-label="Basic example">
+  <button type="button" class="btn text-white" style={{
+      backgroundColor: "#369962"}}>Migrants</button>
+  <button type="button" class="btn text-white" style={{
+      backgroundColor: "#369962"}}>Company</button>
+  <button type="button" class="btn text-white" style={{
+      backgroundColor: "#369962"}}>Login</button>
+</div>
+      <div>
+        <img
           src="https://migracode.openculturalcenter.org/wp-content/uploads/2020/10/Migracode-Barcelona-Logo-Transparant.png"
           style={{ width: "120px" }}
         />
-        <Card.Title>Migracode Project 2022</Card.Title>
-        <Card.Text>
+        <div className="p-2">Migracode Project 2022</div>
+        <div className="p-2">
           Produced by Vanessa Marin - Joao Albitrez - Yhenifer Pulido - Arturo Chavez -
           Jorgelina Donquis.
-        </Card.Text>
+        </div>
          < FaFacebookSquare />
          < BsInstagram />
          < FaTwitterSquare />
          < BsLinkedin />
-      </Card.Body>
-    </Card>
+      </div>
+    </div>
   );
 }
 
