c6e6ee97e3260b040ec4f248021b96646064d821
vanemarinva1983 <vanemarinvalencia@gmail.com>
2022-09-03 23:40:03 +0200

    Pagination_Company

--- Front_End/src/pages/Company.jsx
+++ Front_End/src/pages/Company.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import BannerCompany from '../components/BannerCompany'
+import CardMigrant from '../components/Migrants/CardMigrant'
+import PublicaCompany from '../components/Migrants/PublicaCompany'
+import RegistrarseCompany from '../components/Migrants/RegistrarseCompany'
+import AccedeCompany from '../components/Migrants/AccedeCompany'
+
+
 
 function Company() {
   return (
-    <div>Company</div>
+    <div>
+        <BannerCompany/>
+        <CardMigrant/>
+        <PublicaCompany/>
+        <RegistrarseCompany/>
+        <AccedeCompany/>
+    </div>
   )
 }
 
